6338bf871f2d03a5ae07bff24432289619387526
Evelyn S <elxsham@hotmail.com>
2015-04-30 22:40:02 -0400

    Modified PyGame.py
    Fixed bug preventing script running in command line (removed header
    text).

--- PyGame.py
+++ PyGame.py
@@ -1,7 +1,4 @@
-Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
-    Type "copyright", "credits" or "license()" for more information.
-
-def saystuff(somestring):
-        print("String passed: ", somestring)
+def printSomething(string):
+        print(string)
         
-saystuff("Hello World.")
+printSomething("It Works!")
